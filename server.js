2b37b63e695386a1629b8faf22139ab420ad65b2
fernandogelin <fernandogelin@gmail.com>
2018-08-17 12:49:22 -0400

    removes /app from path in server

--- server.js
+++ server.js
@@ -5,11 +5,10 @@ const express = require('express');
 // Constants
 const PORT = 8080;
 const HOST = '0.0.0.0';
-const APP = 'app';
 
 // App
 const app = express();
 app.use(express.static("."));
 
-app.listen(PORT, HOST, APP);
-console.log(`Running on http://${HOST}:${PORT}/${APP}`);
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
